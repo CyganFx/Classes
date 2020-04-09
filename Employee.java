2a44dc1b0ad0b5e15070d274045998232950a361
CyganFx <62188916+CyganFx@users.noreply.github.com>
2020-04-10 04:29:04 +0600

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,71 @@
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+/**
+ * Employee class template
+ */
+public class Employee {
+    private String name;
+    private double rate;
+    private int hours;
+    private static int totalSumOfHours;
+    private static int employeeCounter;
+    private final DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+    public Employee() {
+        employeeCounter++;
+        totalSumOfHours += hours;
+        decimalFormat.setRoundingMode(RoundingMode.CEILING);
+    }
+
+    public Employee(String name, int rate) {
+        this.name = name;
+        this.rate = rate;
+        employeeCounter++;
+        decimalFormat.setRoundingMode(RoundingMode.CEILING);
+    }
+
+    public Employee(String name, int rate, int hours) {
+        this.name = name;
+        this.rate = rate;
+        this.hours = hours;
+        employeeCounter++;
+        totalSumOfHours += hours;
+        decimalFormat.setRoundingMode(RoundingMode.CEILING);
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public double getRate() { return rate; }
+
+    public void setRate(double rate) { this.rate = rate; }
+
+    public int getHours() { return hours; }
+
+    public void setHours(int hours) {
+        totalSumOfHours = totalSumOfHours + hours - this.hours;
+        this.hours = hours;
+    }
+
+    public static int getTotalSumOfHours() { return totalSumOfHours; }
+
+    public static int getEmployeeCounter() { return employeeCounter; }
+
+    public String salary() {
+        return decimalFormat.format((rate * hours) + bonuses());
+    }
+
+    public double bonuses() {
+        return (rate * hours) * 0.1;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", rate=" + rate +
+                ", hours=" + hours +
+                '}';
+    }
+}
